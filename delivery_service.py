a95cb7090a4197b894c28aeb7844aadc2b914a03
lina1906 <angelina.fefelova190602@gmail.com>
2024-11-17 23:22:56 +0300

    Create delivery_service.py

---
+++ delivery_service.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+def count_min_numbers_of_platforms(weights: List[int], limit: int) -> int:
+    """Считает минимальное количество платформ, для перевозки роботов."""
+    sorted_weights = sorted(weights)
+    lightest = 0
+    heaviest = len(sorted_weights) - 1
+    platforms = 0
+
+    while lightest <= heaviest:
+        if sorted_weights[lightest] + sorted_weights[heaviest] <= limit:
+            lightest += 1
+        heaviest -= 1
+        platforms += 1
+
+    return platforms
+
+
+if __name__ == "__main__":
+
+    weights = list(map(int, input().split()))
+    limit = int(input())
+
+    result = count_min_numbers_of_platforms(weights, limit)
+    print(result)
